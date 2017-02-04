aa893319553afaf773b649ea138a9bc951fcef5d
MidnightDrifter <sean.higgins@digipen.edu>
2017-02-03 18:49:37 -0800

    Compiles with Cygwin on laptop, still lots of errors though

--- main_part1.cpp
+++ main_part1.cpp
@@ -1,11 +1,105 @@
 #include <fstream>   /* ifstream */
 #include <iostream> /*std::cout*/
 #include <cstdio>    /* sscanf */
-#include "pthread.h" /* thread stuff */
-#include "sched.h"
-#include "semaphore.h"
-#include "Project1\Cells.h"
-int rows, cols;
+#include <pthread.h>
+#include <sched.h>
+#include <semaphore.h>
+
+
+
+class Cells
+{
+public:
+
+	Cells(int r, int c)
+	{
+		cellGrid = new bool*[numRows];
+		for (int i = 0; i < numRows; i++)
+		{
+			cellGrid[i] = new bool[numCols];
+			for (int j = 0; j < numCols; j++)
+			{
+				cellGrid[i][j] = false;
+			}
+		}
+	}
+
+	~Cells()
+	{
+		for (int i = 0; i < numRows; i++)
+		{
+			delete[] cellGrid[i];
+		}
+		delete[] cellGrid;
+	}
+	//Clls& operator=(const Cells& c);
+	//void Update();
+
+	int numRows, numCols;
+	bool** cellGrid;
+};
+
+//Implementing the Barrier class in the Lil' Book of Semaphores in C++
+
+class ReusableBarrier
+{
+public:
+	ReusableBarrier(int n) : n(n), count(0) { pthread_mutex_init(&mutex, NULL);  sem_init(&turnstile0, 0, 0);  sem_init(&turnstile1, 0, 0); }
+	~ReusableBarrier() { pthread_mutex_destroy(&mutex);  sem_destroy(&turnstile0); sem_destroy(&turnstile1); }
+
+	void phase1(){
+		pthread_mutex_lock(&mutex);
+		count++;
+		if (count == n)
+		{
+			//Send n signals to let all n threads through
+			for (int i = 0; i < n; i++)
+			{
+				sem_post(&turnstile0);
+			}
+		}
+		pthread_mutex_unlock(&mutex);
+		sem_wait(&turnstile0);
+	}
+
+	void phase2()
+	{
+		pthread_mutex_lock(&mutex);
+		count--;
+		if (count == 0)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				sem_post(&turnstile1);
+			}
+		}
+		pthread_mutex_unlock(&mutex);
+		sem_wait(&turnstile1);
+	}
+
+	void wait()
+	{
+		this->phase1();
+		this->phase2();
+	}
+	int n, count;
+	pthread_mutex_t mutex;
+	sem_t turnstile0, turnstile1;
+
+
+};
+
+
+
+int rows, cols, numThreads;
+pthread_mutex_t mutex;// = new 
+sem_t readBarrier1, readBarrier2, writeBarrier1, writeBarrier2;
+//sem_init(&readBarrier1, 0, 0);
+
+
+
+
+int mutexCount=0;
 struct threadData {
 
 	int myRow, myCol;
@@ -18,10 +112,20 @@ void* Update(void* data)
 	//(int[2]) rowCol = (int[2]) data;
 	//threadData* myData;
 	threadData* myData = (threadData*)data;
-
+	
 	int count = 0;
 	//STOP HERE TOO, everyone only goes when EVERYONE is done writing
+	//Rendezvous point 1 ?
+	//pthread_mutex_lock(&mutex);
+	//mutexCount++;
 
+	//if (mutexCount == numThreads)
+	//{
+	//	
+	//}
+
+
+	//pthread_mutex_unlock(&mutex);
 
 	for (int i = myData->myRow - 1; i <= myData->myRow + 1; i++)
 	{
@@ -50,7 +154,7 @@ void* Update(void* data)
 	{
 		myData->myGrid->cellGrid[myData->myRow][myData->myCol] = true;
 	}
-
+	return 0;
 }
 
 
@@ -62,17 +166,19 @@ int main( int argc, char ** argv )
     }
     
 	int num_iter;
+	std::string outFilename, inputFile;
 	num_iter= 0;
+	std::sscanf(argv[1], "%s", &inputFile);
     std::sscanf(argv[2],"%i",&num_iter);
-
+	std::sscanf(argv[3], "%s", &outFilename);
     // input  file argv[1]
     // output file argv[3]
 
 	int rows, cols, a,b;
 
-	char* inputFile;
-	inputFile= "init0";
-	FILE* fp = fopen(inputFile,"r");
+	//char* inputFile;
+	//inputFile= "init0";
+	FILE* fp = fopen(inputFile.c_str(),"r");
 	fscanf(fp, "%i, %i", &rows, &cols);
 
 	Cells grid(rows, cols);
@@ -83,25 +189,55 @@ int main( int argc, char ** argv )
 		grid.cellGrid[a][b] = true;
 	}
 
-
-	int numThreads = rows*cols;
-
+	fclose(fp);
+	 numThreads = rows*cols;
+	mutexCount = numThreads;
 
 	threadData* data = new threadData;
 	data->myGrid = &grid;
 
 	pthread_t **threads = new pthread_t*[rows];
-	for (int i = 0; i < rows; i++)
+
+	for (int iterations = 0; iterations < num_iter; iterations++)
 	{
-		data->myRow = i;
-		threads[i] = new pthread_t[cols];
-		for (int j = 0; j < cols; j++)
+		for (int i = 0; i < rows; i++)
 		{
-			data->myCol = j;
-			pthread_create(&threads[i][j], NULL, Update, (void*)data);
+			data->myRow = i;
+			if (iterations == 0)
+			{
+				threads[i] = new pthread_t[cols];
+			}
+			for (int j = 0; j < cols; j++)
+			{
+				data->myCol = j;
+				pthread_create(&threads[i][j], NULL, Update, (void*)data);
+			}
 		}
 	}
 
+	for(int i=0;i<rows;i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			pthread_join(threads[i][j], NULL);
+		}
+	}
+
+
+	std::ofstream out;
+	out.open(outFilename.c_str());
+	
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			if (grid.cellGrid[i][j])
+			{
+				out << i << ", " << j << std::endl;
+			}
+		}
+	}
 
 
 }
--- semaphore.h
+++ semaphore.h
@@ -1,41 +1,41 @@
 /*
- * Module: semaphore.h
- *
- * Purpose:
- *	Semaphores aren't actually part of the PThreads standard.
- *	They are defined by the POSIX Standard:
- *
- *		POSIX 1003.1b-1993	(POSIX.1b)
- *
- * --------------------------------------------------------------------------
- *
- *      Pthreads-win32 - POSIX Threads Library for Win32
- *      Copyright(C) 1998 John E. Bossom
- *      Copyright(C) 1999,2005 Pthreads-win32 contributors
- * 
- *      Contact Email: rpj@callisto.canberra.edu.au
- * 
- *      The current list of contributors is contained
- *      in the file CONTRIBUTORS included with the source
- *      code distribution. The list can also be seen at the
- *      following World Wide Web location:
- *      http://sources.redhat.com/pthreads-win32/contributors.html
- * 
- *      This library is free software; you can redistribute it and/or
- *      modify it under the terms of the GNU Lesser General Public
- *      License as published by the Free Software Foundation; either
- *      version 2 of the License, or (at your option) any later version.
- * 
- *      This library is distributed in the hope that it will be useful,
- *      but WITHOUT ANY WARRANTY; without even the implied warranty of
- *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *      Lesser General Public License for more details.
- * 
- *      You should have received a copy of the GNU Lesser General Public
- *      License along with this library in the file COPYING.LIB;
- *      if not, write to the Free Software Foundation, Inc.,
- *      59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
- */
+* Module: semaphore.h
+*
+* Purpose:
+*	Semaphores aren't actually part of the PThreads standard.
+*	They are defined by the POSIX Standard:
+*
+*		POSIX 1003.1b-1993	(POSIX.1b)
+*
+* --------------------------------------------------------------------------
+*
+*      Pthreads-win32 - POSIX Threads Library for Win32
+*      Copyright(C) 1998 John E. Bossom
+*      Copyright(C) 1999,2005 Pthreads-win32 contributors
+*
+*      Contact Email: rpj@callisto.canberra.edu.au
+*
+*      The current list of contributors is contained
+*      in the file CONTRIBUTORS included with the source
+*      code distribution. The list can also be seen at the
+*      following World Wide Web location:
+*      http://sources.redhat.com/pthreads-win32/contributors.html
+*
+*      This library is free software; you can redistribute it and/or
+*      modify it under the terms of the GNU Lesser General Public
+*      License as published by the Free Software Foundation; either
+*      version 2 of the License, or (at your option) any later version.
+*
+*      This library is distributed in the hope that it will be useful,
+*      but WITHOUT ANY WARRANTY; without even the implied warranty of
+*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*      Lesser General Public License for more details.
+*
+*      You should have received a copy of the GNU Lesser General Public
+*      License along with this library in the file COPYING.LIB;
+*      if not, write to the Free Software Foundation, Inc.,
+*      59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+*/
 #if !defined( SEMAPHORE_H )
 #define SEMAPHORE_H
 
@@ -70,11 +70,11 @@
 #endif
 
 /*
- * When building the library, you should define PTW32_BUILD so that
- * the variables/functions are exported correctly. When using the library,
- * do NOT define PTW32_BUILD, and then the variables/functions will
- * be imported correctly.
- */
+* When building the library, you should define PTW32_BUILD so that
+* the variables/functions are exported correctly. When using the library,
+* do NOT define PTW32_BUILD, and then the variables/functions will
+* be imported correctly.
+*/
 #if !defined(PTW32_STATIC_LIB)
 #  if defined(PTW32_BUILD)
 #    define PTW32_DLLPORT __declspec (dllexport)
@@ -86,9 +86,9 @@
 #endif
 
 /*
- * This is a duplicate of what is in the autoconf config.h,
- * which is only used when building the pthread-win32 libraries.
- */
+* This is a duplicate of what is in the autoconf config.h,
+* which is only used when building the pthread-win32 libraries.
+*/
 
 #if !defined(PTW32_CONFIG_H)
 #  if defined(WINCE)
@@ -104,8 +104,8 @@
 #endif
 
 /*
- *
- */
+*
+*/
 
 #if PTW32_SEMAPHORE_LEVEL >= PTW32_SEMAPHORE_LEVEL_MAX
 #if defined(NEED_ERRNO)
@@ -123,41 +123,41 @@ extern "C"
 #endif				/* __cplusplus */
 
 #if !defined(HAVE_MODE_T)
-typedef unsigned int mode_t;
+	typedef unsigned int mode_t;
 #endif
 
 
-typedef struct sem_t_ * sem_t;
+	typedef struct sem_t_ * sem_t;
 
-PTW32_DLLPORT int __cdecl sem_init (sem_t * sem,
-			    int pshared,
-			    unsigned int value);
+	PTW32_DLLPORT int __cdecl sem_init(sem_t * sem,
+		int pshared,
+		unsigned int value);
 
-PTW32_DLLPORT int __cdecl sem_destroy (sem_t * sem);
+	PTW32_DLLPORT int __cdecl sem_destroy(sem_t * sem);
 
-PTW32_DLLPORT int __cdecl sem_trywait (sem_t * sem);
+	PTW32_DLLPORT int __cdecl sem_trywait(sem_t * sem);
 
-PTW32_DLLPORT int __cdecl sem_wait (sem_t * sem);
+	PTW32_DLLPORT int __cdecl sem_wait(sem_t * sem);
 
-PTW32_DLLPORT int __cdecl sem_timedwait (sem_t * sem,
-				 const struct timespec * abstime);
+	PTW32_DLLPORT int __cdecl sem_timedwait(sem_t * sem,
+		const struct timespec * abstime);
 
-PTW32_DLLPORT int __cdecl sem_post (sem_t * sem);
+	PTW32_DLLPORT int __cdecl sem_post(sem_t * sem);
 
-PTW32_DLLPORT int __cdecl sem_post_multiple (sem_t * sem,
-				     int count);
+	PTW32_DLLPORT int __cdecl sem_post_multiple(sem_t * sem,
+		int count);
 
-PTW32_DLLPORT int __cdecl sem_open (const char * name,
-			    int oflag,
-			    mode_t mode,
-			    unsigned int value);
+	PTW32_DLLPORT int __cdecl sem_open(const char * name,
+		int oflag,
+		mode_t mode,
+		unsigned int value);
 
-PTW32_DLLPORT int __cdecl sem_close (sem_t * sem);
+	PTW32_DLLPORT int __cdecl sem_close(sem_t * sem);
 
-PTW32_DLLPORT int __cdecl sem_unlink (const char * name);
+	PTW32_DLLPORT int __cdecl sem_unlink(const char * name);
 
-PTW32_DLLPORT int __cdecl sem_getvalue (sem_t * sem,
-				int * sval);
+	PTW32_DLLPORT int __cdecl sem_getvalue(sem_t * sem,
+		int * sval);
 
 #if defined(__cplusplus)
 }				/* End of extern "C" */
